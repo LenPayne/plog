6589c8947732db7cfdc63c043f418452e70175b0
Len Payne <len.payne@lambtoncollege.ca>
2014-05-28 18:26:34 -0400

    Ported over Original LenPayne.ca

--- web.js
+++ web.js
@@ -1,10 +1,16 @@
-var express = require('express');
+var express = require('express'),
+  path = require('path');
 
 var app = express();
 
 app.get('/', function(req, res) {
-  res.status(404).sendfile('404.html');
+  next('/index.html')
 });
+
+app.get(/^\/([\w\/]*\.(?:html|css|js|gif|png|jpeg|ico))$/, function(req, res) {
+  var filename = path.join('public', req.params[0]);
+  res.sendfile(filename);
+})
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
