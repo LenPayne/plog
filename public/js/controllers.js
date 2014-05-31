fb13aa71e94ebaa6e316d2f5c5db4298024c5564
Len Payne <len.payne@lambtoncollege.ca>
2014-05-31 16:58:03 -0400

    Meaningful Error Messages for Post Submitting

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -23,11 +23,18 @@ plogControllers.controller('PostDetailCtrl', ['$scope', '$routeParams', 'Post',
 
 plogControllers.controller('NewPostCtrl', ['$scope', 'Post',
   function($scope, Post) {
+    $scope.error = '';
+
     $scope.newPost = function() {
       var post = new Post;
       post.apiKey = $('#apiKey').val();
       post.title = $scope.title;
       post.content = $scope.content;
-      post.$save();
+      post.$save(function () {
+        $location.path('/posts/' + post.title);
+      },
+      function(httpResponse) {
+        $scope.error = JSON.stringify(httpResponse);
+      });
     }
   }]);
